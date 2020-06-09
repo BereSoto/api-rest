5e481b5e177fc897fa257dbfb8a8a05ef235d538
BereSoto <beresoto.f@gmail.com>
2020-06-09 17:56:54 -0500

    instalando compile daemon

--- main.go
+++ main.go
@@ -8,35 +8,35 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-// Index is the index handler
+// Index
 func Index(ctx *fasthttp.RequestCtx) {
 	fmt.Fprint(ctx, "no se que estoy haciendo pero funciono \n")
 }
 
-// Hello is the Hello handler
+// Hello
 func Hello(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "hello, %s!\n", ctx.UserValue("name"))
+	fmt.Fprintf(ctx, "hello, %s!\n", ctx.UserValue("hola"))
 }
 
-// MultiParams is the multi params handler
+// MultiParams
 func MultiParams(ctx *fasthttp.RequestCtx) {
 	fmt.Fprintf(ctx, "hi, %s, %s!\n", ctx.UserValue("name"), ctx.UserValue("word"))
 }
 
 // QueryArgs is used for uri query args test #11:
 // if the req uri is /ping?name=foo, output: Pong! foo
-// if the req uri is /piNg?name=foo, redirect to /ping, output: Pong!
+// if the req uri is /piNg?name=foo, redirect to /ping, output: estoy en pign!
 func QueryArgs(ctx *fasthttp.RequestCtx) {
 	name := ctx.QueryArgs().Peek("name")
-	fmt.Fprintf(ctx, "Pong! %s\n", string(name))
+	fmt.Fprintf(ctx, "estoy en ping! %s\n", string(name))
 }
 
 func main() {
 	router := fasthttprouter.New()
 	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
-	router.GET("/multi/:name/:word", MultiParams)
-	router.GET("/ping", QueryArgs)
+	//router.GET("/hello/:hola", Hello)
+	//router.GET("/multi/:name/:word", MultiParams)
+	//router.GET("/ping", QueryArgs)
 
 	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
 }
